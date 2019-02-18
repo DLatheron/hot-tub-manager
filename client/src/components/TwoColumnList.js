fe93ba75d3b2ba700a702a653d4c93566eec3be9
David Latheron <david@latheron.plus.com>
2019-02-18 13:16:21 +0000

    Check-in before removing (or attempting to remove) select controls.

--- client/src/components/TwoColumnList.js
+++ client/src/components/TwoColumnList.js
@@ -1,42 +1,71 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowUp, faArrowDown, faAngleDoubleLeft, faAngleDoubleRight, faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
-import { HTTP_VERSION_NOT_SUPPORTED } from 'http-status-codes';
+import {
+    faAngleDoubleLeft,
+    faAngleDoubleRight,
+    faArrowLeft,
+    faArrowRight,
+    faTimesCircle
+} from '@fortawesome/free-solid-svg-icons'
 
 // TODO:
-// - Support for single of multiple selections???
-// - Data from data structure...
-//   - Support for optgroup
+// x Support for single of multiple selections???
+// x Data from data structure...
+//   x Support for optgroup
 // - Highlight colour support (trick);
-// - Move left;
-// - Move all left;
-// - Move right;
-// - Move all right;
-// - Move up;
-// - Move down;
-// - Autosort;
-// - Preserve order;
-// Filter;
-// Clear filtering;
-// Vertical centering of middle toolbar;
-// Bottom toolbar optional;
-// Titles;
-// Titles optional;
-// Search optional.
-// Change handlers.
-
+// x Move left;
+// x Move all left;
+// x Move right;
+// x Move all right;
+// d Move up;
+// d Move down;
+// x Autosort;
+// x Preserve order;
+// x Filter;
+// x Clear filtering;
+// x Vertical centering of middle toolbar;
+// d Bottom toolbar optional;
+// x Titles;
+// x Titles optional;
+// x Search optional;
+// x Change handlers;
+// - Minimise redrawing...
+// - Customise ordering of the toolbar buttons;
+// - Customisable titles;
+// - Customisable buttons;
+// - Remove reliance on Select.
 export default class TwoColumnList extends React.PureComponent {
     static propTypes = {
+        leftTitle: PropTypes.string,
+        rightTitle: PropTypes.string,
+        leftSearch: PropTypes.bool,
+        rightSearch: PropTypes.bool,
         options: PropTypes.array,
         selected: PropTypes.object,
-        onSelectedChanged: PropTypes.func
+        onSelectedChanged: PropTypes.func,
+        buttonOrder: PropTypes.arrayOf(PropTypes.string)
     };
 
     static defaultProps = {
+        leftTitle: 'All Options:',
+        rightTitle: 'Active:',
+        leftSearch: true,
+        rightSearch: true,
         options: [],
         selected: {},
-        onSelectedChanged: () => {}
+        onSelectedChanged: () => {},
+        buttonOrder: [
+            'MoveAllRight',
+            'MoveRight',
+            'MoveLeft',
+            'MoveAllLeft'
+        ]
+    };
+
+    state = {
+        leftSearchTerm: '',
+        rightSearchTerm: '',
     };
 
     constructor(props) {
@@ -89,6 +118,7 @@ export default class TwoColumnList extends React.PureComponent {
             this.props.selected,
             selectedOptions
         ));
+        this.leftSelectRef.current.selectedIndex = -1;
     }
 
     handleMoveLeft = () => {
@@ -98,6 +128,7 @@ export default class TwoColumnList extends React.PureComponent {
             this.props.selected,
             selectedOptions
         ));
+        this.rightSelectRef.current.selectedIndex = -1;
     }
 
     handleMoveAllRight = (event) => {
@@ -114,18 +145,52 @@ export default class TwoColumnList extends React.PureComponent {
         });
 
         this.props.onSelectedChanged(allSelected);
+        this.leftSelectRef.current.selectedIndex = -1;
     }
 
     handleMoveAllLeft = (event) => {
         this.props.onSelectedChanged({});
+        this.rightSelectRef.current.selectedIndex = -1;
     }
 
-    renderOption = (option, selectionState) => {
+    static filterOption(option, regExp) {
+        if (!regExp) {
+            return option;
+        }
+
+        if (option.text.match(regExp)) {
+            return option;
+        }
+
+        return false;
+    }
+
+    handleSelectChange = () => {
+        this.forceUpdate();
+    }
+
+    canMoveAllToRight() {
+        return Object.keys(this.props.selected).length < 13;
+    }
+
+    canMoveAllToLeft() {
+        return Object.keys(this.props.selected).length > 0;
+    }
+
+    canMoveToRight() {
+        return this.leftSelectRef.current && this.leftSelectRef.current.selectedIndex !== -1;
+    }
+
+    canMoveToLeft() {
+        return this.rightSelectRef.current && this.rightSelectRef.current.selectedIndex !== -1;
+    }
+
+    renderOption = (option, selectionState, regExp) => {
         if (option.values) {
             const subOptions = option.values.reduce((filtered, option) => {
-                const renderResult = this.renderOption(option, selectionState);
+                const renderResult = this.renderOption(option, selectionState, regExp);
                 if (renderResult) {
-                   filtered.push(renderResult);
+                    filtered.push(renderResult);
                 }
                 return filtered;
             }, []);
@@ -142,7 +207,8 @@ export default class TwoColumnList extends React.PureComponent {
                 );
             }
         } else {
-            if ((this.props.selected[option.value] || false) === selectionState) {
+            if ((this.props.selected[option.value] || false) === selectionState
+                && TwoColumnList.filterOption(option, regExp)) {
                 return (
                     <option
                         key={option.value}
@@ -155,49 +221,139 @@ export default class TwoColumnList extends React.PureComponent {
         }
     }
 
+    renderButton = (buttonType) => {
+        switch (buttonType) {
+            case 'MoveAllRight':
+                return (
+                    <button
+                        type='button'
+                        disabled={!this.canMoveAllToRight()}
+                        onClick={this.handleMoveAllRight}
+                    >
+                        <FontAwesomeIcon icon={faAngleDoubleRight} />
+                    </button>
+                );
+
+            case 'MoveAllLeft':
+                return (
+                    <button
+                        type='button'
+                        disabled={!this.canMoveAllToLeft()}
+                        onClick={this.handleMoveAllLeft}
+                    >
+                        <FontAwesomeIcon icon={faAngleDoubleLeft} />
+                    </button>
+                );
+
+            case 'MoveRight':
+                return (
+                    <button
+                        type='button'
+                        disabled={!this.canMoveToRight()}
+                        onClick={this.handleMoveRight}
+                    >
+                        <FontAwesomeIcon icon={faArrowRight} />
+                    </button>
+                );
+
+            case 'MoveLeft':
+                return (
+                    <button
+                        type='button'
+                        disabled={!this.canMoveToLeft()}
+                        onClick={this.handleMoveLeft}
+                    >
+                        <FontAwesomeIcon icon={faArrowLeft} />
+                    </button>
+                );
+
+            default:
+                    throw new Error(`Unknown button type ${buttonType}`);
+        }
+    }
+
     render() {
+        const leftRegExp = this.state.leftSearchTerm && new RegExp(`/${this.state.leftSearchTerm}/i`);
+        const rightRegExp = this.state.rightSearchTerm && new RegExp(`/${this.state.rightSearchTerm}/i`);
+
         return (
             <div className='two-column-list'>
-                <div className='search-bar left'>
-                    <input placeholder='Type search term...' />
-                </div>
-                <div className='search-bar right'>
-                    <input placeholder='Type search term...' />
-                </div>
+                {
+                    this.props.leftTitle &&
+                        <div className='title left'>
+                            {this.props.leftTitle}
+                            <hr/>
+                        </div>
+                }
+                {
+                    this.props.rightTitle &&
+                        <div className='title right'>
+                            {this.props.rightTitle}
+                            <hr/>
+                        </div>
+                }
+                {
+                    this.props.leftSearch &&
+                        <div className='search-bar left'>
+                            <input
+                                type='text'
+                                placeholder='Type search term...'
+                                value={this.state.leftSearchTerm}
+                                onChange={ event => this.setState({ leftSearchTerm: event.target.value }) }
+                            />
+                            <FontAwesomeIcon
+                                className={`clear + ${!this.state.leftSearchTerm ? 'hidden' : ''}`}
+                                icon={faTimesCircle}
+                                onClick={ _ => this.setState({ leftSearchTerm: '' })}
+                            />
+                        </div>
+                }
+                {
+                    this.props.rightSearch &&
+                        <div className='search-bar right'>
+                            <input
+                                type='text'
+                                placeholder='Type search term...'
+                                value={this.state.rightSearchTerm}
+                                onChange={ event => this.setState({ rightSearchTerm: event.target.value }) }
+                            />
+                            <FontAwesomeIcon
+                                className={`clear + ${!this.state.rightSearchTerm ? 'hidden' : ''}`}
+                                icon={faTimesCircle}
+                                onClick={ _ => this.setState({ rightSearchTerm: '' })}
+                            />
+                        </div>
+                }
                 <div className='list left'>
-                    <select ref={this.leftSelectRef} multiple>
+                    <select
+                        ref={this.leftSelectRef}
+                        onChange={this.handleSelectChange}
+                        multiple
+                    >
                         {
                             this.props.options.map(
-                                option => this.renderOption(option, false)
+                                option => this.renderOption(option, false, leftRegExp)
                             )
                         }
                     </select>
                 </div>
                 <div className='list right'>
-                    <select ref={this.rightSelectRef} multiple>
+                    <select
+                        ref={this.rightSelectRef}
+                        onChange={this.handleSelectChange}
+                        multiple
+                    >
                         {
                             this.props.options.map(
-                                option => this.renderOption(option, true)
+                                option => this.renderOption(option, true, rightRegExp)
                             )
                         }
                     </select>
                 </div>
-                <div className='toolbar left'>
-                    {/* Replace with just the number selected? */}
-                    <button type='button'><FontAwesomeIcon icon={faArrowUp} /></button>
-                    <button type='button'><FontAwesomeIcon icon={faArrowDown} /></button>
-                </div>
-                <div className='toolbar right'>
-                    {/* Replace with just the number selected? */}
-                    <button type='button'><FontAwesomeIcon icon={faArrowUp} /></button>
-                    <button type='button'><FontAwesomeIcon icon={faArrowDown} /></button>
-                </div>
                 <div className='vertical-toolbar middle'>
-                    {/* Allow the ordering of these to be customised. */}
-                    <button type='button' onClick={this.handleMoveAllRight}><FontAwesomeIcon icon={faAngleDoubleRight} /></button>
-                    <button type='button' onClick={this.handleMoveRight}><FontAwesomeIcon icon={faArrowRight} /></button>
-                    <button type='button' onClick={this.handleMoveLeft}><FontAwesomeIcon icon={faArrowLeft} /></button>
-                    <button type='button' onClick={this.handleMoveAllLeft}><FontAwesomeIcon icon={faAngleDoubleLeft} /></button>
+                    {
+                        this.props.buttonOrder.map(this.renderButton)
+                    }
                 </div>
             </div>
         );
