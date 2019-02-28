0670c23ade657c27b77e98a97fedc34117c40d55
David Latheron <david@latheron.plus.com>
2019-02-28 17:53:53 +0000

    Remember last selection on a menu.

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -2,16 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretUp, faCaretDown, faCaretRight } from '@fortawesome/free-solid-svg-icons'
+import { faCaretUp, faCaretDown } from '@fortawesome/free-solid-svg-icons'
 
 // TODO:
 // - Option to only have a single sub-menu (at a given level) open;
+// - Open sub-menu to the currently displayed page - when the main menu changes.
 export class Menu {
-    constructor(id, title, options = null, defaultSelection) {
+    constructor(id, title, options = null, defaultSelectionId) {
         this.id = id;
         this.title = title;
         this.options = options;
-        this.defaultSelection = defaultSelection;
+        this.defaultSelectionId = defaultSelectionId;
+    }
+
+    iterate(iterationFn) {
+        function _iterate(menu, parentTree) {
+            iterationFn(menu, parentTree);
+
+            if (menu.options) {
+                parentTree = [...parentTree, menu];
+
+                menu.options.forEach(subMenu => {
+                    _iterate(subMenu, parentTree);
+                });
+            }
+        }
+
+        _iterate(this, []);
     }
 }
 
@@ -74,16 +91,48 @@ export class HeaderMenu extends React.PureComponent {
 export class SideMenu extends React.PureComponent {
     static propTypes = {
         menu: PropTypes.instanceOf(Menu).isRequired,
+        openMenus: PropTypes.object,
         activeMenu: PropTypes.arrayOf(PropTypes.string).isRequired,
         handleClick: PropTypes.func.isRequired
     };
     static defaultProps = {
-        options: []
     };
-    state = {};
+
+    // constructor(props) {
+    //     super(props);
+
+    //     const open = {};
+
+    //     props.menu.iterate((subMenu, parentTree) => {
+    //         if (subMenu.id === this.props.activeMenu[1]) {
+    //             parentTree.forEach(menu => {
+    //                 open[menu.id] = true
+    //             });
+    //         }
+    //     });
+
+    //     this.state.open = open;
+    //     console.log(`Open: ${JSON.stringify(open, null, 4)}`);
+    // }
+
+    // handleClick(subMenu) {
+    //     if (subMenu.options) {
+    //         const open = { ...this.open };
+
+    //         if (open[subMenu.id]) {
+    //             delete open[subMenu.id];
+    //         } else {
+    //             open[subMenu.id] = true;
+    //         }
+
+    //         this.setState({ open });
+    //     } else {
+    //         this.props.handleClick(subMenu);
+    //     }
+    // }
 
     render() {
-        const { menu, activeMenu, handleClick } = this.props;
+        const { menu, openMenus, activeMenu, handleClick } = this.props;
 
         // TODO: Clean-up and allow full recursion of items.
         return (
@@ -95,6 +144,7 @@ export class SideMenu extends React.PureComponent {
                             <SideMenuItem
                                 key={subMenu.id}
                                 {...subMenu}
+                                open={openMenus[subMenu.id]}
                                 active={subMenu.id === activeMenu[1]}
                                 handleClick={handleClick.bind(this, subMenu)}
                             >
@@ -106,6 +156,7 @@ export class SideMenu extends React.PureComponent {
                                                     <SideMenuItem
                                                         key={subSubMenu.id}
                                                         {...subSubMenu}
+                                                        open={openMenus[subSubMenu.id]}
                                                         active={subSubMenu.id === activeMenu[1]}
                                                         handleClick={handleClick.bind(this, subSubMenu)}
                                                     />
@@ -135,9 +186,6 @@ export class SideMenuItem extends React.PureComponent {
         children: null,
         handleClick: () => {}
     };
-    state = {
-        collapsed: true
-    };
 
     canCollapse = () => {
         return this.props.children !== null;
@@ -146,23 +194,19 @@ export class SideMenuItem extends React.PureComponent {
     handleClick = (event) => {
         event.stopPropagation();
 
-        if (this.canCollapse()) {
-            this.setState(state => ({ collapsed: !state.collapsed }));
-        } else {
-            this.props.handleClick(event);
-        }
+        this.props.handleClick(event);
     }
 
     determineIcon = () => {
         return !this.canCollapse() ? null
-            : this.state.collapsed
-                ? faCaretDown
-                : faCaretUp;
+            : this.props.open
+                ? faCaretUp
+                : faCaretDown;
     }
 
     render() {
-        const { children, active, title } = this.props;
-        const { collapsed } = this.state;
+        const { children, active, title, open } = this.props;
+        // const { collapsed } = this.state;
         const icon = this.determineIcon();
 
         return (
@@ -174,7 +218,7 @@ export class SideMenuItem extends React.PureComponent {
                     {
                         icon
                             ? <FontAwesomeIcon icon={icon} />
-                            : <span class='spacer' />
+                            : <span className='spacer' />
                     }
                     <span className='title'>
                         {title}
@@ -182,7 +226,7 @@ export class SideMenuItem extends React.PureComponent {
                 </div>
                 {
                     this.canCollapse() &&
-                        <div className='collapsible-body' style={{ maxHeight: collapsed ? 0 : '5rem' }}>
+                        <div className='collapsible-body' style={{ maxHeight: open ? '6rem' : 0 }}>
                             {children}
                         </div>
                 }
@@ -193,15 +237,20 @@ export class SideMenuItem extends React.PureComponent {
 
 export default class MainPage extends React.PureComponent {
     static propTypes = {
+        menu: PropTypes.instanceOf(Menu).isRequired,
+        defaultMenu: PropTypes.arrayOf(PropTypes.string).isRequired
     };
+
     static defaultProps = {
         menu: new Menu('main', '', [
             new Menu('creative', 'Creative', [
-                new Menu('creative_import', 'Import'),
-                new Menu('creative_adHarvesting', 'Ad Harvesting'),
-                new Menu('creative_metadataQueue', 'Metadata Queue'),
-                new Menu('creative_approvalQueue', 'Approval Queue')
-            ], ['creative', 'creative_import']),
+                new Menu('uas', 'Underlying Ads', [
+                    new Menu('creative_uas_import', 'Import'),
+                    new Menu('creative_uas_adHarvesting', 'Ad Harvesting'),
+                    new Menu('creative_uas_metadataQueue', 'Metadata Queue'),
+                    new Menu('creative_uas_approvalQueue', 'Approval Queue')
+                ])
+            ], 'creative_uas_import'),
             new Menu('reporting', 'Reporting', [
                 new Menu('internal', 'Internal', [
                     new Menu('reporting_internal_dashboard', 'Dashboard'),
@@ -224,30 +273,80 @@ export default class MainPage extends React.PureComponent {
                     new Menu('reporting_exchange_milestones', 'Milestones')
                 ]),
                 new Menu('simple', 'Simple')
-            ], ['reporting', 'reporting_device-partner_dashboard']),
+            ], 'reporting_device-partner_dashboard'),
             new Menu('channels', 'Channels'),
             new Menu('devices', 'Devices'),
             new Menu('admin', 'Admin')
-        ])
+        ]),
+        defaultMenu: ['reporting', 'reporting_internal_kpis']
     };
-    state = {
-        activeMenu: ['reporting', 'reporting_internal_kpis']
-    };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lastSelections: {},
+            activeMenu: props.defaultMenu,
+            ...this.openAppropriateMenus(props.defaultMenu[1])
+        };
+    }
+
+    openAppropriateMenus(menuId) {
+        const openMenus = {};
+
+        this.props.menu.iterate((subMenu, parentTree) => {
+            if (subMenu.id === menuId) {
+                parentTree.forEach(menu => {
+                    openMenus[menu.id] = true
+                });
+            }
+        });
+
+        console.log(`openMenus: ${JSON.stringify(openMenus, null, 4)}`);
+        return { openMenus };
+    }
 
     handleMenuClick = (menu) => {
         console.log(`Menu clicked: ${menu}`);
 
-        this.setState(state => ({
-            activeMenu: menu.defaultSelection || [menu.id, state.activeMenu[1]]
-        }));
+        const defaultSubMenu = this.state.lastSelections[menu.id] || menu.defaultSelectionId;
+        const defaultSelectionId = [menu.id, defaultSubMenu]
+        const activeMenu = defaultSelectionId || [menu.id, this.state.activeMenu[1]];
+
+        if (this.state.activeMenu[0] !== activeMenu[0] || this.state.activeMenu[1] !== activeMenu[1]) {
+            this.setState({ activeMenu });
+        }
+
+        this.setState(this.openAppropriateMenus(activeMenu[1]));
     }
 
     handleSubMenuClick = (subMenu) => {
         console.log(`Sub menu clicked: ${subMenu}`);
 
-        this.setState(state => ({
-            activeMenu: [state.activeMenu[0], subMenu.id]
-        }));
+        if (subMenu.options) {
+            const openMenus = {};
+
+            if (openMenus[subMenu.id]) {
+                delete openMenus[subMenu.id];
+            } else {
+                openMenus[subMenu.id] = true;
+            }
+
+            this.setState({ openMenus });
+        } else {
+            const activeMenu = [this.state.activeMenu[0], subMenu.id];
+
+            if (this.state.activeMenu[0] !== activeMenu[0] || this.state.activeMenu[1] !== activeMenu[1]) {
+                this.setState({ activeMenu });
+            }
+
+            this.setState({
+                lastSelections: {
+                    ...this.state.lastSelections,
+                    [this.state.activeMenu[0]]: subMenu.id
+                }
+            });
+        }
     }
 
     render() {
@@ -270,6 +369,7 @@ export default class MainPage extends React.PureComponent {
                         <SideMenu
                             menu={sideMenu}
                             activeMenu={this.state.activeMenu}
+                            openMenus={this.state.openMenus}
                             handleClick={this.handleSubMenuClick}
                         />
                 }
